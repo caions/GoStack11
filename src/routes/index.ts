aad969c7c4360eb4d1bff3b40a58eb7cc04cb588
Caio Nascimento <caionsantos2000@gmail.com>
2021-06-05 13:05:40 -0300

    feat: initial config typescript + express

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import {Router} from 'express'
+
+const routes = Router()
+
+routes.get('/',(req,res)=>{
+res.send("Gloria a Deus")
+})
+
+export default routes
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import routes from './routes'
+
+const app = express()
+const PORT = 3000
+
+app.use('/',routes)
+
+app.listen(PORT,()=>console.log("Servidor rodando na porta "+PORT))
