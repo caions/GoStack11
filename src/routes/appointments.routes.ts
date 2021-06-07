b22c9ed83a8f6c2611171af9416d31922c6b7553
Caio Nascimento <caionsantos2000@gmail.com>
2021-06-06 23:29:28 -0300

    feat: criado o service dependency inversion

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,30 +1,30 @@
 import { Router } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
 import { AppointmentsRepository } from '../repositories/AppointmentsRepository';
+import { CreateAppointmentService } from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
 const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (req, res) => {
-  const { provider, date } = req.body;
-  const parsedDate = startOfHour(parseISO(date));
+  try {
+    const { provider, date } = req.body;
+    const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate =
-    appointmentsRepository.findByDate(parsedDate);
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
 
-  if (findAppointmentInSameDate) {
-    return res
-      .status(400)
-      .json({ message: 'The appointment hour is not available.' });
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
-
-  return res.json(appointment);
 });
 
 appointmentsRouter.get('/', (req, res) => {
