e6fb122b203f866563fc725ee048edec044b6cf7
Caio Nascimento <caionsantos2000@gmail.com>
2021-06-16 23:27:44 -0300

    feat: model appointment

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,13 +1,14 @@
 import { v4 } from 'uuid';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 export class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
-  provider: string;
-  date: Date;
 
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = v4();
-    this.provider = provider;
-    this.date = date;
-  }
+  @Column()
+  provider: string;
+
+  @Column('time with time zone')
+  date: Date;
 }
