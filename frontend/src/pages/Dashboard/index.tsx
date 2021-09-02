fd41fbc6c953a59bd72d70f21157900c560d3207
Caio Nascimento <caionsantos2000@gmail.com>
2021-09-02 18:11:40 -0300

    feat: dashboard style

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/28688721?v=4" alt="Caio Nascimento" />
+          <div>
+            <strong>rocketseat</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/28688721?v=4" alt="Caio Nascimento" />
+          <div>
+            <strong>rocketseat</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/28688721?v=4" alt="Caio Nascimento" />
+          <div>
+            <strong>rocketseat</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+      </Repositories>
+    </>
+  );
 };
 
 export { Dashboard };
