1f1da5ffae5852f5f62241deeb24da33e6ccf5a0
Caio Nascimento <caionsantos2000@gmail.com>
2021-09-14 11:38:54 -0300

    feat: estilizando detalhe

--- frontend/src/pages/Repository/index.tsx
+++ frontend/src/pages/Repository/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
-import { Title } from './styles';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, Issues, RepositoryInfo } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -9,9 +12,50 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
   return (
-    <Title>
-      Repository:{params.repository}
-    </Title>
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+
+        <Link to="/"><FiChevronLeft size={16} />Voltar</Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="https://lh3.googleusercontent.com/ogw/ADea4I5KlmxH0TsgXkcqNZ10wBIG1rpiih9uGkTrUzMi=s32-c-mo" alt="imagem" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues Abertos</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link
+          to="asdfds"
+        >
+
+          <div>
+            <strong>fullname</strong>
+
+            <p>description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
